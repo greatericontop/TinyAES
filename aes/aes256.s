290c1ba99b512afc2449d82f876a7a05290f27d3
greatericontop <greateric@protonmail.com>
2025-12-17 15:25:28 -0500

    finish key schedule

--- aes/aes256.s
+++ aes/aes256.s
@@ -20,8 +20,202 @@ aes256_key_expansion:
     movdqu [rsi], xmm0
     movdqu [rsi + 16], xmm1
 
-    # R2, R3 generation
+    # R2: Familiar xor with A from aeskeygenassist
+    aeskeygenassist xmm2, xmm1, 0x01
+    pshufd xmm2, xmm2, 0xff  # A stored in all bits of xmm2
 
+    movdqa xmm3, xmm0
+    pxor xmm0, xmm2  # w0^A w1^A w2^A w3^A
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3  # w0^A w1^w0^A w2^w1^w0^A w3^w2^w1^w0^A
+
+    # R3 SubWord
+    pxor xmm5, xmm5  # this is our zero "round key"
+    pshufd xmm2, xmm0, 0xff  # xmm2 = w3 w3 w3 w3 repeated
+    aesenclast xmm2, xmm5  # subword and shiftword on xmm2
+    pshufd xmm2, xmm2, 0xff  # put unshifted xmm2 back into all of xmm2
+
+    # Now do standard thing
+    movdqa xmm3, xmm1
+    pxor xmm1, xmm2
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+
+    movdqu [rsi + 32], xmm0
+    movdqu [rsi + 48], xmm1
+
+    # R4, R5
+    aeskeygenassist xmm2, xmm1, 0x02
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm0
+    pxor xmm0, xmm2
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+
+    pxor xmm5, xmm5
+    pshufd xmm2, xmm0, 0xff
+    aesenclast xmm2, xmm5
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm1
+    pxor xmm1, xmm2
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+
+    movdqu [rsi + 64], xmm0
+    movdqu [rsi + 80], xmm1
+
+    # R6, R7
+    aeskeygenassist xmm2, xmm1, 0x04
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm0
+    pxor xmm0, xmm2
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+
+    pxor xmm5, xmm5
+    pshufd xmm2, xmm0, 0xff
+    aesenclast xmm2, xmm5
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm1
+    pxor xmm1, xmm2
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+
+    movdqu [rsi + 96], xmm0
+    movdqu [rsi + 112], xmm1
+
+    # R8, R9
+    aeskeygenassist xmm2, xmm1, 0x08
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm0
+    pxor xmm0, xmm2
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+
+    pxor xmm5, xmm5
+    pshufd xmm2, xmm0, 0xff
+    aesenclast xmm2, xmm5
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm1
+    pxor xmm1, xmm2
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+
+    movdqu [rsi + 128], xmm0
+    movdqu [rsi + 144], xmm1
+
+    # R10, R11
+    aeskeygenassist xmm2, xmm1, 0x10
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm0
+    pxor xmm0, xmm2
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+
+    pxor xmm5, xmm5
+    pshufd xmm2, xmm0, 0xff
+    aesenclast xmm2, xmm5
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm1
+    pxor xmm1, xmm2
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+
+    movdqu [rsi + 160], xmm0
+    movdqu [rsi + 176], xmm1
+
+    # R12, R13
+    aeskeygenassist xmm2, xmm1, 0x20
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm0
+    pxor xmm0, xmm2
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+
+    pxor xmm5, xmm5
+    pshufd xmm2, xmm0, 0xff
+    aesenclast xmm2, xmm5
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm1
+    pxor xmm1, xmm2
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+    pslldq xmm3, 4
+    pxor xmm1, xmm3
+
+    movdqu [rsi + 192], xmm0
+    movdqu [rsi + 208], xmm1
+
+    # R14
+    aeskeygenassist xmm2, xmm1, 0x40
+    pshufd xmm2, xmm2, 0xff
+
+    movdqa xmm3, xmm0
+    pxor xmm0, xmm2
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+    pslldq xmm3, 4
+    pxor xmm0, xmm3
+
+    movdqu [rsi + 224], xmm0
 
     leave
     ret
